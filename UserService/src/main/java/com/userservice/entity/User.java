5d684e3c93dc420c1483175ebea153611f825428
Saurabh Kumar <1355saurabh@gmail.com>
2024-04-30 17:53:14 +0530

    user entity basic created.

--- UserService/src/main/java/com/userservice/entity/User.java
+++ UserService/src/main/java/com/userservice/entity/User.java
@@ -1,4 +1,5 @@
 package com.userservice.entity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,9 +19,13 @@ import lombok.Setter;
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
 	private String userId;
+	@Column(name = "NAME")
 	private String name;
+	@Column(name = "EMAIL")
 	private String email;
+	@Column(name = "ABOUT")
 	private String about;
 	
 }
